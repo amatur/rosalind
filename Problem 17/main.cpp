721f2a7e206e04c5556fca12b6405e03f9a84cdf
amatur <amatur003@gmail.com>
2019-03-11 02:01:54 -0400

    done

--- Problem 17/main.cpp
+++ Problem 17/main.cpp
@@ -1,4 +1,4 @@
-// pcov
+// grep
 
 #include <iostream>
 #include <fstream>
@@ -15,6 +15,9 @@ using namespace std;
 
 
 vector <string>  kmerList;
+int glob;
+int line2;
+set<string> results;
 
 
 string purifyString(string s){
@@ -56,7 +59,7 @@ int countEdges(map<string, vector<string> > adjList){
 
 
 void printGraph(map<string, vector<string> > adjList){
-
+    cout<<"PRINTING GRAPH: "<<endl;
         for (map<string, vector<string> > ::iterator it = adjList.begin(); it != adjList.end(); it++ )
         {
             //cout<<it->first<<" :::: ";
@@ -69,38 +72,67 @@ void printGraph(map<string, vector<string> > adjList){
             for (vector<string> :: iterator i = v.begin(); i != v.end(); ++i){
                 cout << '(' << it->first <<", " << *i << ')'<<endl;
             }
+
             //cout<<endl;
         }
 }
 
+void printVector(vector<string> v){
+    for (size_t i = 0; i < v.size(); i++) {
+        cout<<v[i]<< " ";
+    }
+    cout<<endl;
 
-vector<string> DFS(int k, map<string, vector<string> > adjList, string v, vector<string> circuitSoFar){
+}
+
+
+vector<string> DFS(int k, map<string, vector<string> > & adjList, string v, vector<string> circuit){
+    glob++;
+    //cout<<glob<<endl;
     //cout<<"call";
     //printGraph(adjList);
-    vector<string> circuit(circuitSoFar.begin(),circuitSoFar.end());
+    vector<string> circuitLocal(circuit.begin(),circuit.end());
     //cout<<adjList[v].size();
     if(adjList[v].size() != 0){
-        int i = 0;
+
         int n = adjList[v].size() ;
+        int i = n-1;
         while(true){
             //cout<<i<<endl;
-            if(i==n) break;
+            if(i==-1) break;
             string neighborString = adjList[v].at(i);
             //remove edge
-            circuit.push_back(adjList[v].at(i));
+            circuitLocal.push_back(adjList[v].at(i));
+
+            adjList[v].erase(adjList[v].begin() + i);
+
+            //if(line2==10) cout<<adjList["CA"][1]<<" SSXXXXXS"<<endl;
+
             //map<string, vector<string> > adjListCopy;
             //adjListCopy.insert(adjList.begin(), adjList.end());
-            adjList[v].erase(adjList[v].begin() + i);
+            //adjListCopy[v] = neighList;
             //cout<<adjList[v].size();
             //edgeCount[s.nodeLabel]--;
 
+            //vector<string> neighList2;
+            //for (int x=0; x<adjList[neighborString].size(); x++)
+            //  neighList2.push_back(adjList[neighborString][x]);
             //printGraph(adjList);
-            DFS(k, adjList, neighborString, circuit);
+
+            //if(glob<100)
+            DFS(k, adjList, neighborString, circuitLocal);
             //cout<<"after"<<endl;
             //printGraph(adjList);
             adjList[v].push_back(neighborString);
+            circuitLocal.pop_back();
+            // while(!circuitLocal.empty()){
+            //     circuitLocal.pop_back();
+            // }
+            // for (size_t x = 0; x < circuit.size(); x++) {
+            //     circuitLocal.push_back(circuit[]);
+            // }
 
-            i++;
+            i--;
 
             //add edge back
         }
@@ -108,10 +140,24 @@ vector<string> DFS(int k, map<string, vector<string> > adjList, string v, vector
         //circuit.push_back(v);
         //
         //print circuit so circuitSo circuitSoFar
-        for (int i = 0; i<circuitSoFar.size(); i++) {
-            cout<<circuitSoFar.at(i)<< " ";
+        line2++;
+
+        if(circuitLocal.size() == kmerList.size()+1){
+            for (int i = 0; i<circuitLocal.size(); i++) {
+                //cout<<circuitLocal.at(i)<< " ";
+            }
+            string out = "";
+            for (int i = 0; i<kmerList.size(); i++) {
+                out+=circuitLocal.at(i)[0];
+            }
+            results.insert(out);
+
+            //cout<<out<<endl;
+
         }
-        cout<<endl;
+
+        //printGraph(adjList);
+        //cout<<endl;
        // Back-tracking
     }
     return circuit;
@@ -121,36 +167,40 @@ vector<string> DFS(int k, map<string, vector<string> > adjList, string v, vector
 
 
 void assemble(int k, map<string, vector<string> > adjList){
-    string start = kmerList.at(0).substr(1, k-1);
+
+    string startVertex = kmerList.at(0).substr(1, k-1);
+    //cout<<startVertex;
+
     vector<string> ck;
-    ck.push_back(start);
+    ck.push_back(kmerList.at(0).substr(0, k-1));
+    ck.push_back(startVertex);
 
-    vector<string>::iterator it = find(adjList[kmerList.at(0).substr(0, k-1)].begin(), adjList[kmerList.at(0).substr(0, k-1)].end(), start);
+    vector<string>::iterator it = find(adjList[kmerList.at(0).substr(0, k-1)].begin(), adjList[kmerList.at(0).substr(0, k-1)].end(), startVertex);
+    //
     adjList[kmerList.at(0).substr(0, k-1)].erase(it);
-    printGraph(adjList);
+     //printGraph(adjList);
 
-    vector<string> vec = DFS(k, adjList, start, ck);
-    for (int i = 0; i<vec.size(); i++) {
-        cout<<vec.at(i)<< " ";
-    }
-    cout<<endl;
+
+    vector<string> vec = DFS(k, adjList, startVertex, ck);
+}
+
+bool hasEdge(string from, string to, int k){
+    string overlapFrom = from.substr(1, k-1);
+    string overlapTo = to.substr(0, k-1);
+    return overlapFrom.compare(overlapTo)==0;
 }
 
 map<string, vector<string> > makeDBGfromKmers(vector<string> kmerList, int k){
+
     map<string, vector<string> > adjList; //simple cycle
 
-    for(vector<string>:: iterator it=kmerList.begin(); it!=kmerList.end(); ++it){
+
+    for(vector<string>:: iterator it=kmerList.begin() ; it!=kmerList.end(); ++it){
         string kmer = *it;
-        //cout<<"main: "<<k1mer<<" ";
 
-        string kmerFrom, kmerTo;
+        string kmerFrom = kmer.substr(0, k-1);
+        string kmerTo = kmer.substr(1, k-1);
 
-        kmerFrom = kmer.substr(0, k-1);
-        kmerTo = kmer.substr(1, k-1);
-
-        // if(adjList.count(kmerFrom)==0){
-        //     adjList[kmerFrom] = vector<string>();
-        // }
         adjList[kmerFrom].push_back(kmerTo);
         //cout<<kmerFrom<<" -> "<<kmerTo<<endl;
         //added
@@ -158,14 +208,6 @@ map<string, vector<string> > makeDBGfromKmers(vector<string> kmerList, int k){
             adjList[kmerTo] = vector<string>();
         }
 
-        string kmerToRC = reverseComplement(kmerTo);
-        string kmerFromRC = reverseComplement(kmerFrom);
-
-        //cout<<kmerToRC<<" -> "<<kmerFromRC<<endl;
-        adjList[kmerToRC].push_back(kmerFromRC);
-        if(adjList.count(kmerFromRC)==0){
-            adjList[kmerFromRC] = vector<string>();
-        }
     }
 
     return adjList;
@@ -191,9 +233,13 @@ int main() {
     }
 
     adjList = makeDBGfromKmers(kmerList, k); //edge: (k+1) mer, node: k mer, however k is +1
-//printGraph(adjList);
-    assemble(k, adjList);
+    //printGraph(adjList);
+   assemble(k, adjList);
 
+   for(set<string> :: iterator it=results.begin(); it!=results.end(); ++it){
+       cout<<*it<<endl;
+   }
+ // printGraph(adjList);
 
     return 0;
 }
